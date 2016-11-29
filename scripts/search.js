883d583d045031d6dd67be4eb0021aeab7b8fb2e
Kyle H. Lam <kylehl1@uci.edu>
2016-11-29 04:58:40 -0800

    trying

--- scripts/search.js
+++ scripts/search.js
@@ -23,8 +23,8 @@ $(document).ready(function(){
 			return false;
 		}
 		else {
-			//var entry = document.getElementById('query').value;
-			//search(entry);
+			var entry = document.getElementById('query').value;
+			search(entry);
 		}
     });
 });
@@ -43,5 +43,27 @@ function search(entry) {
 
 // Called automatically with the response of the YouTube API request.
 function onSearchResponse(data) {
-    showResponse(data);
+    success: function (data) {
+        var row = "";
+        for (i = 0; i < data.feed.entry.length; i++) {
+            row += "<div class='search_item'>";
+            row += "<table width='100%'>";
+            row += "<tr>";
+            row += "<td vAlign='top' align='left'>";
+            row += "<a href='#' ><img width='120px' height='80px' src=" + data.feed.entry[i].media$group.media$thumbnail[0].url + " /></a>";
+            row += "</td>";
+            row += "<td vAlign='top' width='100%' align='left'>";
+            row += "<a href='#' ><b>" + data.feed.entry[i].media$group.media$title.$t + "</b></a><br/>";
+            row += "<span style='font-size:12px; color:#555555'>by " + data.feed.entry[i].author[0].name.$t + "</span><br/>";
+            row += "<span style='font-size:12px' color:#666666>" + data.feed.entry[i].yt$statistics.viewCount + " views" + "<span><br/>";
+            row += "</td>";
+            row += "</tr>";
+            row += "</table>";
+            row += "</div>";
+        }
+        document.getElementById("search-results-block").innerHTML = row;
+    },
+    error: function () {
+        alert("Error loading youtube video results");
+    }
 }
