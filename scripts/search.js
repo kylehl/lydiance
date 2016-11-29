faeafe34c46e82ec1b76c18c8731b2fd342c0745
Kyle H. Lam <kylehl1@uci.edu>
2016-11-28 23:29:04 -0800

    fixing search.js

--- scripts/search.js
+++ scripts/search.js
@@ -12,14 +12,14 @@ function makeRequest(){
             maxResults: 10
     });
     request.execute(function(response)  {                                                                                    
-            $('#results').empty()
-            var srchItems = response.result.items;                      
-            $.each(srchItems, function(index, item){
-            vidTitle = item.snippet.title;  
-            vidThumburl =  item.snippet.thumbnails.default.url;                 
-            vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
+		$('#results').empty();
+		var srchItems = response.result.items;                      
+		$.each(srchItems, function(index, item){
+		vidTitle = item.snippet.title;  
+		vidThumburl =  item.snippet.thumbnails.default.url;                 
+		vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
 
-            $('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
-    })  
-  })  
+		$('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');                      
+		})
+	})  
 }
