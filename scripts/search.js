0862db1bf904d61001dd2df308550f62c9d692f1
Kyle H. Lam <kylehl1@uci.edu>
2016-11-29 18:14:40 -0800

    trying new search parse

--- scripts/search.js
+++ scripts/search.js
@@ -36,28 +36,13 @@ function search(entry) {
 }
 
 // Called automatically with the response of the YouTube API request.
-function onSearchResponse(data) {
-    success: function (data) {
-        var row = "";
-        for (i = 0; i < data.feed.entry.length; i++) {
-            row += "<div class='search_item'>";
-            row += "<table width='100%'>";
-            row += "<tr>";
-            row += "<td vAlign='top' align='left'>";
-            row += "<a href='#' ><img width='120px' height='80px' src=" + data.feed.entry[i].media$group.media$thumbnail[0].url + " /></a>";
-            row += "</td>";
-            row += "<td vAlign='top' width='100%' align='left'>";
-            row += "<a href='#' ><b>" + data.feed.entry[i].media$group.media$title.$t + "</b></a><br/>";
-            row += "<span style='font-size:12px; color:#555555'>by " + data.feed.entry[i].author[0].name.$t + "</span><br/>";
-            row += "<span style='font-size:12px' color:#666666>" + data.feed.entry[i].yt$statistics.viewCount + " views" + "<span><br/>";
-            row += "</td>";
-            row += "</tr>";
-            row += "</table>";
-            row += "</div>";
-        }
-        document.getElementById("search-results-block").innerHTML = row;
-    },
-    error: function () {
-        alert("Error loading youtube video results");
-    }
+function onSearchResponse(response) {
+    $('#results').empty();
+	var srchItems = response.result.items;                      
+	$.each(srchItems, function(index, item){
+	vidTitle = item.snippet.title;  
+	vidThumburl =  item.snippet.thumbnails.default.url;                 
+	vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:204px;height:128px"></pre>';                   
+
+	$('#results').append('<pre>' + vidTitle + vidThumbimg +   '</pre>');
 }
