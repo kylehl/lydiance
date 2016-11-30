a4bd348479517bc1af520c97d592fe85bfb8b3c2
Kyle H. Lam <kylehl1@uci.edu>
2016-11-30 00:52:52 -0800

    adding ready for click detect

--- scripts/lydiance_form.js
+++ scripts/lydiance_form.js
@@ -1,12 +1,14 @@
 // Check if form is empty
-$('#search-button').click(function() {
-    if ($.trim($("#query").val()) === '') {
-        alert('You did not fill out the required fields.');
-        return false;
-    }
-	else {
-		var entryText = document.getElementById('query').value;
-		$('.ytlist').slideDown("fast");
-		onClickSearch(entryText);
-	}
+$(document).ready(function() {
+	$('#search-button').click(function() {
+		if ($.trim($("#query").val()) === '') {
+			alert('You did not fill out the required fields.');
+			return false;
+		}
+		else {
+			var entryText = document.getElementById('query').value;
+			$('.ytlist').slideDown("fast");
+			onClickSearch(entryText);
+		}
+	});
 });
