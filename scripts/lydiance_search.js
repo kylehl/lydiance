9abe5d68be56db568fe275ac7a347f520ee6e05f
Kyle H. Lam <kylehl1@uci.edu>
2016-12-01 02:04:38 -0800

    new api call for video id

--- scripts/lydiance_search.js
+++ scripts/lydiance_search.js
@@ -17,9 +17,18 @@ function onYouTubeApiLoad() {
 
 // Below is the chain of functions from clicking Go in the search bar
 function onClickSearch(entry) {
-    // Use the JavaScript client library to create a search.list() API call.
+    // for snippet info
     var request = gapi.client.youtube.search.list({
         part: 'snippet',
+        q: entry,
+		maxResults: 10,
+		type: 'video',
+		videoCategoryId: 10 // search for music only
+    });
+	
+	// for id info
+    var requestId = gapi.client.youtube.search.list({
+        part: 'id',
         q: entry,
 		maxResults: 10,
 		type: 'video',
@@ -28,23 +37,31 @@ function onClickSearch(entry) {
     
     // Send the request to the API server,
     // and invoke onSearchResponse() with the response.
+	requestId.execute(getVideoId);
     request.execute(onSearchResponse);
 }
 
 // Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    $('#results').empty();
+var sVideoIds = []; // global var array for search data
+
+// API call for id search
+function getVideoId(response) {
 	var srchItems = response.result.items;
 	$.each(srchItems, function(index, item){
-		vidTitle = item.snippet.title;  
-		vidThumburl =  item.snippet.thumbnails.high.url;
-		vidId = 'placeholder';
-		vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:auto;height:20%"><button type="button" class="btn btn-lg btn-default search-result" href="#contact" id="image-select" value="'+vidId+'">Select</button></pre>';                   
-
-		$('#results').append('<pre>' + vidTitle + vidThumbimg + '</pre>');
+		sVideoIds[i++] = item.id.videoId;
 	});
 }
 
-// next API call from snippet search to id search
-function getVideoId() {
+function onSearchResponse(response) {
+    $('#results').empty();
+	var srchItems = response.result.items;
+	
+	$.each(srchItems, function(index, item){
+		vidTitle = item.snippet.title;
+		vidThumburl =  item.snippet.thumbnails.high.url;
+		vidId = sVideoIds[index];
+		vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:auto;height:20%"><button type="button" class="btn btn-lg btn-default search-result" name="image-select" id="image-select" value="'+vidId+'">Select</button></pre>';                   
+
+		$('#results').append('<pre>' + vidTitle + vidThumbimg + '</pre>');
+	});
 }
