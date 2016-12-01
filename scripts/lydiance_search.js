a718dd4cc5a45eae6907686aefc0d185b7749c78
Kyle H. Lam <kylehl1@uci.edu>
2016-12-01 04:35:34 -0800

    adding console.log as debug

--- scripts/lydiance_search.js
+++ scripts/lydiance_search.js
@@ -49,6 +49,7 @@ function getVideoId(response) {
 	var srchItems = response.result.items;
 	$.each(srchItems, function(index, item){
 		sVideoIds[index] = item.id.videoId;
+		console.log(sVideoIds[index]);
 	});
 }
 
@@ -60,6 +61,7 @@ function onSearchResponse(response) {
 		var vidTitle = item.snippet.title;
 		var vidThumburl =  item.snippet.thumbnails.high.url;
 		var vidId = sVideoIds[index];
+		console.log(vidId);
 		var vidThumbimg = '<pre><img id="thumb" src="'+vidThumburl+'" alt="No  Image  Available." style="width:auto;height:20%"><button type="button" class="btn btn-lg btn-default search-result" name="image-select" id="image-select" value="'+vidId+'">Select</button></pre>';                   
 
 		$('#results').append('<pre>' + vidTitle + vidThumbimg + '</pre>');
